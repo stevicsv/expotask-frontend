f812f82807c34c89150707639cd545edd65e7cad
Veljko Stević <stevic.sv@gmail.com>
2021-04-07 23:28:59 +0200

    add auth repository

---
+++ src/repositories/auth.js
@@ -0,0 +1,10 @@
+import defaultAxios from 'axios';
+import axios from '@/plugins/axios';
+
+// Endpoints
+const createSession = () => defaultAxios.get('http://localhost:8080/sanctum/csrf-cookie', { withCredentials: true });
+const login = (credentials) => axios.post('auth/login', credentials);
+const register = (credentials) => axios.post('auth/register', credentials);
+
+// Exports
+export { createSession, login, register };
