2c495ebc8c22702c88bc32c483c3a393ed984e3d
Veljko Stević <stevic.sv@gmail.com>
2021-04-09 22:05:12 +0200

    finish login page style

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,12 +1,66 @@
 <template>
   <div class="login">
-    <form @submit.prevent="login">
-      <input type="email" v-model="form.email" />
-      <span v-if="form.errors.email">{{ form.errors.email[0] }}</span>
-      <input type="password" v-model="form.password" />
-      <span v-if="form.errors.password">{{ form.errors.password[0] }}</span>
-      <button type="submit">Submit</button>
-    </form>
+    <div class="login__header">
+      <div class="login__header-logo">
+        <Logo class="logo" />
+        <div class="vertical-line"></div>
+        <p>Sign in page</p>
+      </div>
+
+      <div class="login__header-create-account">
+        <h4>New to Expotask?</h4>
+        <button class="is-secondary">Create an Account</button>
+      </div>
+    </div>
+
+    <div class="form-wrapper">
+      <Logo class="logo mobile-logo" />
+
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label class="form-label">Email address</label>
+          <input class="form-input" type="email" v-model="form.email" />
+          <span class="form-feedback is-error" v-if="form.errors.email">
+            {{ form.errors.email[0] }}
+          </span>
+        </div>
+
+        <div class="form-group">
+          <label class="form-label with-link">
+            Password
+            <a href="#">Forgot Password?</a>
+          </label>
+          <input class="form-input" type="password" v-model="form.password" />
+          <span class="form-feedback is-error" v-if="form.errors.password">
+            {{ form.errors.password[0] }}
+          </span>
+        </div>
+
+        <div class="form-group">
+          <button
+            type="submit"
+            role="button"
+            class="is-primary"
+            :class="{ 'is-loading': form.loading }"
+            :disabled="form.loading"
+          >
+            <div class="loader" v-if="form.loading" />
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+
+    <div class="bottom">
+      <div class="mobile-only">
+        <a href="#">Create an Account</a>
+        <div class="horizontal-line"></div>
+      </div>
+
+      <p class="copyright">
+        © Copyright {{ new Date().getFullYear() }}, Expotask
+      </p>
+    </div>
   </div>
 </template>
 
@@ -14,8 +68,10 @@
 import { defineComponent, reactive } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
+import Logo from '@/components/Logo.vue';
 
 export default defineComponent({
+  components: { Logo },
   setup() {
     const store = useStore();
     const router = useRouter();
@@ -24,17 +80,19 @@ export default defineComponent({
       email: '',
       password: '',
       errors: [],
+      loading: false,
     });
 
     const login = async () => {
+      form.loading = true;
       try {
         await store.dispatch('login', form);
         router.push({ name: 'Dashboard' });
       } catch ({ errors }) {
         form.errors = errors;
       } finally {
-        form.email = '';
         form.password = '';
+        form.loading = false;
       }
     };
 
