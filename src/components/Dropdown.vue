a48ab3e7b876da6717f0a76edf867989b9021de9
Veljko Stević <stevic.sv@gmail.com>
2021-04-12 23:35:12 +0200

    add Dropdown component & implement on dashboard page

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dropdown">
+    <button class="dropdown-trigger" @click="toggle">
+      <slot name="trigger" />
+    </button>
+    <div class="dropdown-menu" v-if="visible">
+      <slot name="menu" />
+      <span class="triangle"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export const Dropdown = defineComponent({
+  setup() {
+    const visible = ref(false);
+
+    const toggle = () => {
+      visible.value = !visible.value;
+    };
+
+    return {
+      visible,
+      toggle,
+    };
+  },
+});
+
+export const DropdownItem = {
+  render() {
+    return <div class="dropdown-item">{this.$slots.default()}</div>;
+  },
+};
+
+export default Dropdown;
+</script>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -11,31 +11,30 @@
       <div class="dashboard__header-autharea">
         <button class="is-smaller is-secondary">Create Team</button>
         <div class="vertical-line"></div>
-        <h4 @click="toggle" style="display: block; margin-right: 10px">
-          <div v-if="user">
-            {{ user.name }}
-            <svg
-              class="chevron"
-              viewBox="0 0 11 7"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M0.292786 1.29299C0.480314 1.10552 0.734622 1.0002 0.999786 1.0002C1.26495 1.0002 1.51926 1.10552 1.70679 1.29299L4.99979 4.58599L8.29279 1.29299C8.38504 1.19748 8.49538 1.1213 8.61739 1.06889C8.73939 1.01648 8.87061 0.988893 9.00339 0.987739C9.13617 0.986585 9.26785 1.01189 9.39074 1.06217C9.51364 1.11245 9.62529 1.1867 9.71918 1.28059C9.81308 1.37449 9.88733 1.48614 9.93761 1.60904C9.98789 1.73193 10.0132 1.86361 10.0121 1.99639C10.0109 2.12917 9.9833 2.26039 9.93089 2.38239C9.87848 2.5044 9.8023 2.61474 9.70679 2.70699L5.70679 6.70699C5.51926 6.89446 5.26495 6.99978 4.99979 6.99978C4.73463 6.99978 4.48032 6.89446 4.29279 6.70699L0.292786 2.70699C0.105315 2.51946 0 2.26515 0 1.99999C0 1.73483 0.105315 1.48052 0.292786 1.29299Z"
-                fill="currentColor"
-              />
-            </svg>
-          </div>
-        </h4>
-
-        <div class="dropdown" v-if="visible">
-          <li class="dropdown__item">Profile</li>
-          <li class="dropdown__item" @click="logout">Logout</li>
-          <span class="triangle" />
-        </div>
-      </div>
+        <Dropdown>
+          <template #trigger>
+            <h4 v-if="user">
+              {{ user.name }}
+              <svg
+                class="chevron"
+                viewBox="0 0 11 7"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M0.292786 1.29299C0.480314 1.10552 0.734622 1.0002 0.999786 1.0002C1.26495 1.0002 1.51926 1.10552 1.70679 1.29299L4.99979 4.58599L8.29279 1.29299C8.38504 1.19748 8.49538 1.1213 8.61739 1.06889C8.73939 1.01648 8.87061 0.988893 9.00339 0.987739C9.13617 0.986585 9.26785 1.01189 9.39074 1.06217C9.51364 1.11245 9.62529 1.1867 9.71918 1.28059C9.81308 1.37449 9.88733 1.48614 9.93761 1.60904C9.98789 1.73193 10.0132 1.86361 10.0121 1.99639C10.0109 2.12917 9.9833 2.26039 9.93089 2.38239C9.87848 2.5044 9.8023 2.61474 9.70679 2.70699L5.70679 6.70699C5.51926 6.89446 5.26495 6.99978 4.99979 6.99978C4.73463 6.99978 4.48032 6.89446 4.29279 6.70699L0.292786 2.70699C0.105315 2.51946 0 2.26515 0 1.99999C0 1.73483 0.105315 1.48052 0.292786 1.29299Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </h4>
+          </template>
+          <template #menu>
+            <DropdownItem>Profile</DropdownItem>
+            <DropdownItem @click="logout">Logout</DropdownItem>
+          </template>
+        </Dropdown>
     </div>
   </div>
 </template>
@@ -45,9 +44,10 @@ import { defineComponent, computed, ref } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 import Logo from '@/components/Logo.vue';
+import { Dropdown, DropdownItem } from '@/components/Dropdown.vue';
 
 export default defineComponent({
-  components: { Logo },
+  components: { Logo, Dropdown, DropdownItem },
   setup() {
     const store = useStore();
     const router = useRouter();
@@ -62,11 +62,16 @@ export default defineComponent({
       router.push({ name: 'Login' });
     };
 
+    const redirect = (name) => {
+      router.push({ name });
+    };
+
     return {
       user: computed(() => store.state.user),
       logout,
       visible,
       toggle,
+      redirect,
     };
   },
 });
